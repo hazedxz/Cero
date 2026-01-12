65dfa0c141a68284aed8f043b12ff7e98509217c
hazedxz <tumblr1275@gmail.com>
2026-01-12 18:20:58 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ const nextConfig = {
   images: {
     domains: ['media.giphy.com', 'media0.giphy.com', 'media1.giphy.com', 'media2.giphy.com', 'media3.giphy.com', 'media4.giphy.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
+
 module.exports = nextConfig
